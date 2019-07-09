ec2c4d30c3b0a3d853ddf1aafde852fe8422c15d
Pera <tqlook@qq.com>
2019-07-09 15:58:03 +0800

    abcd

---
+++ app/src/main/java/com/practicaltraining/render/views/SimpleToolBar.java
@@ -0,0 +1,10 @@
+package com.practicaltraining.render.views;
+
+import android.content.Context;
+import android.widget.Toolbar;
+
+public class SimpleToolBar extends Toolbar {
+    public SimpleToolBar(Context context) {
+        super(context);
+    }
+}
